e963b1ebf3bb79aa777abefe42b1d6d76f4604a7
yushi koga <youshe.old.pleasure@is-tech.co.jp>
2019-08-12 15:13:00 +0900

    page 188

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Vue from "vue";
+
+export type Todo = {
+  id: string;
+  task: string;
+  done: boolean;
+};
+export type Data = {
+  firstName: string;
+  lastName: string;
+  todos: Todo[];
+};
+export default Vue.extend({
+  data: (): Data => ({
+    firstName: "Taro",
+    lastName: "Yamada",
+    todos: []
+  })
+});
+</script>
