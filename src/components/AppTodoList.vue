9e4c144380efa6e86c0a76c1a4520a1f643c1c8e
yushi koga <youshe.old.pleasure@is-tech.co.jp>
2019-08-12 15:21:28 +0900

    page 190

--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -1,21 +1,21 @@
+<template>
+  <div class="todos">
+    <div v-for="todo in todos" :key="todo.id">
+      <AppTodoItem :todo="todo" />
+    </div>
+  </div>
+</template>
 <script lang="ts">
 import Vue from "vue";
+import AppTodoItem, { Todo } from "./AppTodoItem.vue";
 
-export type Todo = {
-  id: string;
-  task: string;
-  done: boolean;
-};
 export type Data = {
-  firstName: string;
-  lastName: string;
   todos: Todo[];
 };
 export default Vue.extend({
   data: (): Data => ({
-    firstName: "Taro",
-    lastName: "Yamada",
     todos: []
-  })
+  }),
+  components: { AppTodoItem }
 });
 </script>
